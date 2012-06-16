d71ac421581aab0fea7d34ac3ce6cfb3b6bdc04a
zhuangxm <zxm@basecity.com>
2012-06-17 00:44:38 +0800

    optimize performance

--- src/learn_algorithm/divide_conquer.clj
+++ src/learn_algorithm/divide_conquer.clj
@@ -1,6 +1,6 @@
 (ns learn-algorithm.divide-conquer)
 
-(defn conquer [coll1 coll2]
+(defn conquer-old [coll1 coll2]
   (loop [c* 0 rs* [] c1* coll1 c2* coll2] 
     (cond
      (or (empty? c1*) (empty? c2*)) [c* (concat rs* c1* c2*)]
@@ -9,6 +9,18 @@
      :else (recur (+ c* (count c1*))
                   (conj rs* (first c2*)) c1* (rest c2*)))))
 
+(defn conquer [coll1 coll2]
+  (let [c1 (count coll1)
+        c2 (count coll2)]
+    (loop [c* 0 rs* [] i 0 j 0]
+      (cond
+       (>= i c1) [c* (concat rs* (drop j coll2))]
+       (>= j c2) [c* (concat rs* (drop i coll1))]
+       (<= (nth coll1 i) (nth coll2 j))
+       (recur c* (conj rs* (nth coll1 i)) (inc i) j)
+       :else (recur (+ c* (- c1 i)) (conj rs* (nth coll2 j))
+                    i (inc j))))))
+
 (defn caculate [coll]
   (let [c (count coll)]
     (if (== c 1)
@@ -16,7 +28,7 @@
       (let [n (/ c 2)
             [n1 c1] (caculate (take n coll))
             [n2 c2] (caculate (drop n coll))
-            [t sc] (conquer c1 c2)]
+            [t sc] (conquer (vec c1) (vec c2))]
         [(+ n1 n2 t) sc]))))
 
 (defn caculate-inversion [f]
@@ -25,4 +37,3 @@
          (map #(Integer/parseInt %))
          (caculate)
          (first))))
-
